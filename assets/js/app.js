481cbcd41dc53642f2d3a1946025eac15bbd60f4
Augustin Grisel <aug.gris@gmail.com>
2020-03-26 06:20:37 +0700

    :heavy_plus_sign: add bootstrap

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,6 +6,7 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
+require('bootstrap')
 import '../css/app.css';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
